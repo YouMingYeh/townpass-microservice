0905931353e47d5decccb10b310a68e411ebfbe6
Claire <claire@Clairedebijixingdiannao.local>
2024-09-08 04:04:41 +0800

    .

--- apps/app/src/components/MapComponent.tsx
+++ apps/app/src/components/MapComponent.tsx
@@ -193,7 +193,7 @@ export const MapComponent = ({
                 onClick={() => setSelectedReport(report)}
                 animation={window.google.maps.Animation.DROP}
                 label={{
-                  text: report.emoji,
+                  text: report.emoji || '🙂'
                 }}
                 title={report.username}
               />
